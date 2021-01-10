75633896d007ab66b9730b601c7cba7fb916b787
br3d <13rihon@gmail.com>
2021-01-10 19:52:43 +0200

    feature: add entrypoint

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,20 +3,7 @@
 echo "INFO: R-Node startup..."
 
 echo "INFO: NETWORK_TYPE = ${NETWORK_TYPE}"
-echo "INFO: CONFIGS_PATH = ${CONFIGS_PATH}"
-echo "INFO: \$1 = $1"
 
-cd /home/ton/ton-node
-exec /ton-node/ton_node --configs "${CONFIGS_PATH}"
+./ton_node --configs configs/ >>logs/ton.log
 
 echo "INFO: R-Node startup... DONE"
-
-# curl -sS "https://raw.githubusercontent.com/tonlabs/${NETWORK_TYPE}/master/configs/${NETWORK_TYPE}/ton-global.config.json" \
-#     -o "${CONFIGS_PATH}/ton-global.config.json"
-
-# if [ "$1" = "bash" ]; then
-#     tail -f /dev/null
-# else
-#     cd /ton-node
-
-# fi
